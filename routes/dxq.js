64a35ca78664350df4309443ad16366f73b8ac20
deyu <zhuzheyu@bytedance.com>
2021-07-07 10:06:45 +0800

    fix: C端接口

--- routes/dxq.js
+++ routes/dxq.js
@@ -125,7 +125,7 @@ router.get('/interviewByCompany', (req, res, next) => {
   });
 });
 
-// 根据公司获取评论列表 参数pid
+// 根据公司获取评论列表 参数cid
 router.get('/commentByCompany', (req, res, next) => {
   conn.connect(function (err) {
     if (err) {
@@ -137,6 +137,120 @@ router.get('/commentByCompany', (req, res, next) => {
   const sql =
     'select * from comment left join student on comment.sid = student.sid where cid=?';
   const sqlParams = [req.query.cid];
+  conn.query(sql, sqlParams, (err, result) => {
+    if (err) {
+      res.json({
+        code: 500,
+        msg: 'sql执行错误',
+        err,
+      });
+    } else {
+      res.json({
+        code: 200,
+        msg: '查询成功',
+        data: result,
+      });
+    }
+  });
+});
+
+/*****************************/
+// 获取学生信息表 参数phone
+router.get('/student', (req, res, next) => {
+  conn.connect(function (err) {
+    if (err) {
+      console.log('[query] - :' + err);
+      return;
+    }
+    console.log('[connection connect]  succeed!');
+  });
+  const sql = 'select * from student where s_phone=?';
+  const sqlParams = [req.query.phone];
+  conn.query(sql, sqlParams, (err, result) => {
+    if (err) {
+      res.json({
+        code: 500,
+        msg: 'sql执行错误',
+        err,
+      });
+    } else {
+      res.json({
+        code: 200,
+        msg: '查询成功',
+        data: result,
+      });
+    }
+  });
+});
+
+// 获取所有项目信息
+router.get('/project', (req, res, next) => {
+  conn.connect(function (err) {
+    if (err) {
+      console.log('[query] - :' + err);
+      return;
+    }
+    console.log('[connection connect]  succeed!');
+  });
+  const sql =
+    'select * from project left join company on project.cid = company.cid';
+  conn.query(sql, (err, result) => {
+    if (err) {
+      res.json({
+        code: 500,
+        msg: 'sql执行错误',
+        err,
+      });
+    } else {
+      res.json({
+        code: 200,
+        msg: '查询成功',
+        data: result,
+      });
+    }
+  });
+});
+
+// 获取学生接过的项目 参数sid
+router.get('/projectByStudent', (req, res, next) => {
+  conn.connect(function (err) {
+    if (err) {
+      console.log('[query] - :' + err);
+      return;
+    }
+    console.log('[connection connect]  succeed!');
+  });
+  const sql =
+    'select * from (sp left join project on sp.pid = project.pid) left join company on project.cid = company.cid where sid=?';
+  const sqlParams = [req.query.sid];
+  conn.query(sql, sqlParams, (err, result) => {
+    if (err) {
+      res.json({
+        code: 500,
+        msg: 'sql执行错误',
+        err,
+      });
+    } else {
+      res.json({
+        code: 200,
+        msg: '查询成功',
+        data: result,
+      });
+    }
+  });
+});
+// 根据学生获取面试信息 参数sid
+router.get('/interviewByStudent', (req, res, next) => {
+  conn.connect(function (err) {
+    if (err) {
+      console.log('[query] - :' + err);
+      return;
+    }
+    console.log('[connection connect]  succeed!');
+  });
+  const sql =
+    'select * from interview left join company on interview.cid = company.cid where sid=?';
+  const sqlParams = [req.query.sid];
   conn.query(sql, sqlParams, (err, result) => {
     if (err) {
       res.json({
